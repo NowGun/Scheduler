f8f8ed6fede7c93f75748133515685fa3829a3ec
Daniil Zhirov <nowgun98@gmail.com>
2022-05-18 19:15:45 +0300

    добавлены закладки

--- Scheduler/Pages/BookMark.xaml.cs
+++ Scheduler/Pages/BookMark.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Scheduler.DataBaseClass;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +27,37 @@ namespace Scheduler.Pages
         {
             InitializeComponent();
         }
+        private async void LoadListBox()
+        {
+            ObservableCollection<ListMark> co = new();
+
+            using schedulerContext db = new();
+
+            var list = await db.Cases.Where(l => l.CaseBookmark == 1).ToListAsync();
+
+            foreach (var item in list)
+            {
+                co.Add(new ListMark 
+                {
+                    header = item.CaseTitle,
+                    info = item.CaseDescription,
+                    date = item.CaseDate.ToString(),
+                    time = $"{item.CaseTimestart} - {item.CaseTimeend}"
+                });
+            }
+            ListBoxCaseMark.ItemsSource = co;
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadListBox();
+        }
+    }
+    partial class ListMark
+    {
+        public string? time { get; set; }
+        public string? info { get; set; }
+        public string? header { get; set; }
+        public string? date { get; set; }
     }
 }
